4669f08984cfe203c5fffe33ea5733af79ab5222
Dylan Powers <ddp19@duke.edu>
2020-02-27 19:16:41 -0800

    Updates

---
+++ precache-manifest.d78dab4539c982562eaecb7faaec1f94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ff1f56ce4e3eaf77bf0303e313f996",
+    "url": "/jtdp/index.html"
+  },
+  {
+    "revision": "5a89561e254b4c35472e",
+    "url": "/jtdp/static/css/2.5ffa1744.chunk.css"
+  },
+  {
+    "revision": "224f2dd105f27f98ec7c",
+    "url": "/jtdp/static/css/main.cb32a638.chunk.css"
+  },
+  {
+    "revision": "5a89561e254b4c35472e",
+    "url": "/jtdp/static/js/2.83da3e5b.chunk.js"
+  },
+  {
+    "revision": "78ae9ef58bd98eea99e01ab52d988667",
+    "url": "/jtdp/static/js/2.83da3e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224f2dd105f27f98ec7c",
+    "url": "/jtdp/static/js/main.465a620b.chunk.js"
+  },
+  {
+    "revision": "5cd7af2c1a40a8a98119",
+    "url": "/jtdp/static/js/runtime-main.a7bc4d3d.js"
+  }
+]);
