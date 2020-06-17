45adf8ad7920f39a7f5dbb168e4da6737b5fd6a8
Dylan Powers <ddp19@duke.edu>
2020-06-16 23:14:35 -0400

    Moved all configs into env variables

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "jtdp-60b3d.firebaseapp.com",
-  databaseURL: "https://jtdp-60b3d.firebaseio.com",
-  projectId: "jtdp-60b3d",
-  storageBucket: "jtdp-60b3d.appspot.com",
-  messagingSenderId: "786415873557",
-  appId: "1:786415873557:web:defecb2e86a857bf8b281c"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const DYLAN_PICS_DIRNAME = 'dylan-pics/';
